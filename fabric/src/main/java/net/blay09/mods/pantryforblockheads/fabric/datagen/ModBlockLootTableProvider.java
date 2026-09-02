ab119717b74748f0175748b80fe07e31aa1cfbed
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-09-02 13:17:11 +0200

    refactor(26.3): Temporarily disable silk touch in loot tables

--- fabric/src/main/java/net/blay09/mods/pantryforblockheads/fabric/datagen/ModBlockLootTableProvider.java
+++ fabric/src/main/java/net/blay09/mods/pantryforblockheads/fabric/datagen/ModBlockLootTableProvider.java
@@ -102,7 +102,7 @@ public class ModBlockLootTableProvider extends FabricBlockLootSubProvider {
     }
 
     private LootItemCondition.Builder hasAnyShearsToolOrSilkTouch() {
-        return hasAnyShearsTool().or(() -> hasSilkTouch().value());
+        return hasAnyShearsTool(); // TODO .or(() -> hasSilkTouch().value());
     }
 
     private LootItemCondition.Builder doesNotHaveAnyShearsToolOrSilkTouch() {
